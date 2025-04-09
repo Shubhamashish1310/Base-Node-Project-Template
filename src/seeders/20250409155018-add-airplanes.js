8fd448f421a5f751f690b897c9e3aee9bbdc038b
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2025-04-09 22:41:12 +0530

    Fix formatting in airplane seeder rollback function

--- src/seeders/20250409155018-add-airplanes.js
+++ src/seeders/20250409155018-add-airplanes.js
@@ -42,6 +42,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-    await queryInterface.bulkDelete('Airplanes', null, {});>>>>
+    await queryInterface.bulkDelete('Airplanes', null, {});
   }
 };
